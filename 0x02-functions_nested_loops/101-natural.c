a66b5a354af1e848be232264de7dd5223b25b2b1
lroudge <lauradrc@hotmail.fr>
2019-02-12 17:50:36 +0000

    Added advanced task 13 to project 0x02 C

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,20 +7,23 @@
  */
 int main(void)
 {
-	unsigned long int i, j, k, sum;
+	unsigned long int i, sum3, sum5, sum;
+	
+	sum3 = 0;
+	sum5 = 0;
+	sum = 0;
 
-	for (i = 0; i < 1024; i++)
+	for (i = 0; i < 1024; ++i)
 	{
-		j = i / 3;
-		k = i / 5;
 		if ((i % 3) == 0)
 		{
-			sum = sum + j;
+			sum3 = sum3 + i;
 		} else if ((i % 5) == 0)
 		{
-			sum = sum + k;
+			sum5 = sum5 + i;
 		}
 	}
+	sum = sum3 + sum5;
 	printf("%lu\n", sum);
 	return (0);
 }
