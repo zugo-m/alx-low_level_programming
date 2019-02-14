c442b43407c6cdf049fbf099f648c7395e2c2509
lroudge <lauradrc@hotmail.fr>
2019-02-14 21:48:58 +0000

    Added task 12 to project 0x03 C

---
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+	}
+	
+	if (n == 0)
+	{
+		_putchar('0');
+	} else
+	{
+		if (n / 10)
+		{
+			print_number(n / 10);
+		}
+		_putchar((n % 10) + '0');
+	}
+}
