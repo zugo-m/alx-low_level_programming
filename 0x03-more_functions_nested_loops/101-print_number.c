cd36c10f69fbb7d146dc271a4e316d7df7d4f9ee
lroudge <lauradrc@hotmail.fr>
2019-02-14 21:51:29 +0000

    Changed advanced task 12

--- 0x03-more_functions_nested_loops/101-print_number.c
+++ 0x03-more_functions_nested_loops/101-print_number.c
@@ -11,16 +11,10 @@ void print_number(int n)
 		_putchar('-');
 		n = -n;
 	}
-	
-	if (n == 0)
+
+	if (n / 10)
 	{
-		_putchar('0');
-	} else
-	{
-		if (n / 10)
-		{
-			print_number(n / 10);
-		}
-		_putchar((n % 10) + '0');
+		print_number(n / 10);
 	}
+	_putchar((n % 10) + '0');
 }
