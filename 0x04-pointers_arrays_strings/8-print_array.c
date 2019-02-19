d3fdb55d6addb1d11b965d3fa2059835c4e79427
lroudge <lauradrc@hotmail.fr>
2019-02-19 17:30:53 +0000

    Changed task 8 of project 0x04 C

--- 0x04-pointers_arrays_strings/8-print_array.c
+++ 0x04-pointers_arrays_strings/8-print_array.c
@@ -11,14 +11,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	if (n > 0)
-	{
-		printf("%d", a[0]);
+	printf("%d", a[0]);
 
-		for (i = 1; i < n; i++)
-		{
-			printf(", %d", a[i]);
-		}
-		printf("\n");
+	for (i = 1; i < n; i++)
+	{
+		printf(", %d", a[i]);
 	}
+		printf("\n");
 }
