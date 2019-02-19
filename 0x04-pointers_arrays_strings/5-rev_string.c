52a4f970a03c276501c4ee0f3b44b3538517cc4e
lroudge <lauradrc@hotmail.fr>
2019-02-19 17:05:13 +0000

    Changed tasks 0, 5 and 6 of project 0x04 C

--- 0x04-pointers_arrays_strings/5-rev_string.c
+++ 0x04-pointers_arrays_strings/5-rev_string.c
@@ -7,21 +7,22 @@
 void rev_string(char *s)
 {
 	char tmp;
-	int i, len;
+	int i, len, len1;
 
 	len = 0;
+	len1 = 0;
 
 	while (s[len] != '\0')
 	{
 		len++;
 	}
 
-	len--;
+	len1 = len - 1;
 
 	for (i = 0; i < len / 2; i++)
 	{
 		tmp = s[i];
-		s[i] = s[len];
-		s[len--] = tmp;
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
 }
