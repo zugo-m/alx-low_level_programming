1ca248ab41340af4c04cbb0e25b4db46e64c1534
lroudge <lauradrc@hotmail.fr>
2019-02-21 22:53:53 +0000

    Added task 6 to project 0x05 C

---
+++ 0x05-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * cap_string - capitalizes everey word of a string
+ * @s: string to modify
+ *
+ * Return: the resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j, len;
+
+	char spe[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	len = 0;
+
+	while (s[len] != '\0')
+		len++;
+	
+	for (i = 0; i < len; i++)
+	{
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spe[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+	}
+
+	return (s);
+}
