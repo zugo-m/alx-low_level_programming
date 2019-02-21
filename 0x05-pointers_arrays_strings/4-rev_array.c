01fabeda33c951860f5374500caed626ab56e163
lroudge <lauradrc@hotmail.fr>
2019-02-21 21:41:43 +0000

    Changed task 4 of project 0x04 C

--- 0x05-pointers_arrays_strings/4-rev_array.c
+++ 0x05-pointers_arrays_strings/4-rev_array.c
@@ -3,7 +3,7 @@
 /**
  * reverse_array - reverses an array of integers
  * @a: array to be reversed
- * n: number of elements in the array
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
@@ -11,7 +11,7 @@ void reverse_array(int *a, int n)
 
 	j = n - 1;
 
-	for (i = 0; i <= n / 2; i++)
+	for (i = 0; i < n / 2; i++)
 	{
 		tmp = a[i];
 		a[i] = a[j];
