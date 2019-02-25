b9c3c56f75239fcf027b9768853dd4b622f6da3a
lroudge <lauradrc@hotmail.fr>
2019-02-25 20:34:25 +0000

    Initial commit of project 0x06 C

---
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include "holberton.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search
+ * @c: char to find
+ *
+ * Return: a pointer to the first occurrence of the character
+ * c in the string s, or NULL if the character is not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+
+	return (NULL);
+}
