2b8731473c449b01c0f976d9526f2dd0483e1ffe
lroudge <lauradrc@hotmail.fr>
2019-02-25 21:51:32 +0000

    Added task 5 of project 0x06

---
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * *_strstr - locates a substring
+ * @haystack: string to search in
+ * @needle: substring to look for
+ *
+ * Return: pointer to the beginning of the located substring
+ * or NULL if the substring is not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i] != '\0'; i++)
+	{
+		for (j = 0; haystack[j] != '\0'; j++)
+		{
+			if (needle[i] == haystack[j])
+			{
+				return (haystack + j);
+			}
+		}
+	}
+
+	return (NULL);
+}
