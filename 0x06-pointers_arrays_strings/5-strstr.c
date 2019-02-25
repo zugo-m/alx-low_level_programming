c69c7d9f04624f65f5d08dda2a611d8349993b2f
lroudge <lauradrc@hotmail.fr>
2019-02-25 22:44:46 +0000

    Added task 5 to project 0x06

--- 0x06-pointers_arrays_strings/5-strstr.c
+++ 0x06-pointers_arrays_strings/5-strstr.c
@@ -13,16 +13,15 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i] != '\0'; i++)
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j] != '\0'; j++)
+		for (j = 0; needle[j] != '\0'; j++)
 		{
-			if (needle[i] == haystack[j])
-			{
-				return (haystack + j);
-			}
+			if (haystack[i + j] != needle[j])
+				break;
 		}
+		if (!needle[j])
+			return (&haystack[i]);
 	}
-
 	return (NULL);
 }
