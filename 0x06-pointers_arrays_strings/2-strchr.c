d7e90fe9f32ed6c0f4fa6ca6793329bb9751032f
lroudge <lauradrc@hotmail.fr>
2019-02-26 22:18:15 +0000

    Changed task 2 to a simpler syntax

--- 0x06-pointers_arrays_strings/2-strchr.c
+++ 0x06-pointers_arrays_strings/2-strchr.c
@@ -11,19 +11,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a;
-
-	while (1)
+	while (*s)
 	{
-		a = *s++;
-		if (a == c)
-		{
-			return (s - 1);
-		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
-
+		if (*s == c)
+			return (s);
+		s++;
 	}
+
+	return (NULL);
 }
