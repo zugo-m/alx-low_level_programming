fcfaafae3c1cca36506061ec2e875b220e32fd54
lroudge <lauradrc@hotmail.fr>
2019-02-28 03:41:11 +0000

    Small correction on task 8

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -35,8 +35,7 @@ int wildcmp(char *s1, char *s2)
 		if (*s1 == *s2)
 			sum += wildcmp(s1 + 1, s2 + 1);
 		sum += bandersnatch(s1 + 1, s2);
-		if (sum >= 1)
-			return (1);
+		return (!!sum);
 	}
 	return (0);
 }
