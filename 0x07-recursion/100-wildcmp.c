3902ec09fabdb3707e26172bb06990e5f7504f9f
lroudge <lauradrc@hotmail.fr>
2019-02-28 01:24:39 +0000

    Added advanced task 8 to project 0x07 C

---
+++ 0x07-recursion/100-wildcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+ 
+/**
+ * wildcmp - compares two strings
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 1 if the strings can be considered identical
+ * otherwise 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+		return (0);
+	if (*s2 == '\0')
+		return (0);
+	if (*s2 == '*')
+		return (wildcmp(s1 + 1, s2));
+	if (*s1 == *(s2 + 1) && *s1 != *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*(s2 + 1) == '*')
+		return (wildcmp(s1, s2 + 1));
+	if (*s1 != *s2)
+		return (wildcmp(s1 + 1, s2 + 1));
+	return(1);
+}
