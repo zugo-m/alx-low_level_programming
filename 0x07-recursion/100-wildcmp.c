2fa9ce6ac20475d9b79983dadbb88e34bf294ad2
lroudge <lauradrc@hotmail.fr>
2019-02-28 03:39:28 +0000

    Small correction on task 8

--- 0x07-recursion/100-wildcmp.c
+++ 0x07-recursion/100-wildcmp.c
@@ -52,7 +52,7 @@ int wildcmp(char *s1, char *s2)
 int bandersnatch(char *s1, char *s2)
 {
 	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
+		return (wildcmp(s1, s2));
 	return (bandersnatch(s1 + 1, s2));
 }
 
