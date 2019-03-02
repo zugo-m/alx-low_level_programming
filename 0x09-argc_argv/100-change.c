140066eb1e911f997cff4498d2646bf815d385ab
lroudge <lauradrc@hotmail.fr>
2019-03-02 05:23:04 +0000

    Refactored the advanced task 5

--- 0x09-argc_argv/100-change.c
+++ 0x09-argc_argv/100-change.c
@@ -13,51 +13,41 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, num, result;
+	int num, i, j, result;
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num = atoi(argv[1]);
+	result = 0;
+
+	if (argv[1][0] == '-')
+	{
+		puts("0");
+		return (0);
+	}
 
 	for (i = 0; argv[1][i] != '\0'; i++)
 	{
-		if (argv[1][i] < '0' || argv[1][i] > '9')
+
+		if ((argv[1][i] < '0' || argv[1][i] > '9') && argv[1][0] != '-')
 		{
 			puts("Error");
 			return (1);
 		}
 	}
 
-	result = 0;
-	num = atoi(argv[1]);
-
-	while (num > 0)
+	for (j = 0; j < 5 && num > 0; j++)
 	{
-		if (num >= 25)
+		if (num >= coins[j])
 		{
-			num -= 25;
-			result++;
-		} else if (num >= 10)
-		{
-			num -= 10;
-			result++;
-		} else if (num >= 5)
-		{
-			num -= 5;
-			result++;
-		} else if (num >= 2)
-		{
-			num -= 2;
-			result++;
-		} else if (num == 1)
-		{
-			num -= 1;
+			num -= coins[j];
 			result++;
 		}
 	}
-
 	printf("%d\n", result);
 	return (0);
 }
