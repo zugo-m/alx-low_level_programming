44dbbfccbed377b4e0c338db237e609135eca877
lroudge <lauradrc@hotmail.fr>
2019-03-05 22:00:39 +0000

    Find the error in task 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -26,7 +26,10 @@ int **alloc_grid(int width, int height)
 	{
 		arr[i] = (int *) malloc(sizeof(int) * width);
 		if (arr[i] == NULL)
+		{
+			free(arr);
 			return (NULL);
+		}
 	}
 
 	for (i = 0; i < height; i++)
@@ -36,6 +39,5 @@ int **alloc_grid(int width, int height)
 			arr[i][j] = 0;
 		}
 	}
-
 	return (arr);
 }
