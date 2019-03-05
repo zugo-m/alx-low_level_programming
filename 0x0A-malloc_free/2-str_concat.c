603441b657e970e229e486b649505508462dcf32
lroudge <lauradrc@hotmail.fr>
2019-03-05 21:25:34 +0000

    Modification in task 2

--- 0x0A-malloc_free/2-str_concat.c
+++ 0x0A-malloc_free/2-str_concat.c
@@ -11,39 +11,39 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *s3;
-	unsigned int i = 0, j = 0, len = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1[len])
-		len++;
-	while (s2[len])
-		len++;
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
 
-	s3 = malloc(sizeof(char) * (len + 2));
+	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
 	if (s3 == NULL)
 		return (NULL);
 
-	if (s1 == NULL)
-	{
-		while ((s3[i++] = s2[j++]) != '\0')
-			;
-	}
 	i = 0;
 	j = 0;
-	if (s2 == NULL)
+
+	if (s1)
 	{
-		while ((s3[i++] = s1[j++]) != '\0')
-			;
-	}
-	i = 0;
-	j = 0;
-	while (s1[i])
-	{
-		s3[i] = s1[i];
-		i++;
+		while (i < len1)
+		{
+			s3[i] = s1[i];
+			i++;
+		}
 	}
 
-	while ((s3[i++] = s2[j++]) != '\0')
-		;
+	if (s2)
+	{
+		while (i < (len1 + len2))
+		{
+			s3[i] = s2[j];
+			i++;
+			j++;
+		}
+	}
+	s3[i] = '\0';
 
 	return (s3);
 }
