5706790ed5f399b117a0e9afca86f68c6116d3eb
lroudge <lauradrc@hotmail.fr>
2019-03-05 21:40:52 +0000

    Add a condition to task 3

--- 0x0A-malloc_free/3-alloc_grid.c
+++ 0x0A-malloc_free/3-alloc_grid.c
@@ -14,6 +14,9 @@ int **alloc_grid(int width, int height)
 	int **arr;
 	int i, j;
 
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
 	arr = (int **) malloc(sizeof(int *) * height);
 
 	if (arr == NULL)
