76c3232bb47ccece8b6ba36dd93a02b7e724adcf
lroudge <lauradrc@hotmail.fr>
2019-03-07 20:11:50 +0000

    Add a function prototype to the header

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -2,6 +2,7 @@
 #define HOLBERTON_H
 
 void *malloc_checked(unsigned int b);
+char *_memset(char *s, char b, unsigned int n);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
