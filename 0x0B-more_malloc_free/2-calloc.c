740867669508eb934d9d3fd37b39dfd9fd1af8cc
lroudge <lauradrc@hotmail.fr>
2019-03-07 20:23:16 +0000

    Fix a bug in task 2

--- 0x0B-more_malloc_free/2-calloc.c
+++ 0x0B-more_malloc_free/2-calloc.c
@@ -35,12 +35,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(size * (nmemb + 1));
+	ptr = malloc(size * nmemb);
 
 	if (ptr == NULL)
 		return (NULL);
 
-	_memset(ptr, 0, nmemb);
+	_memset(ptr, 0, nmemb * size);
 
 	return (ptr);
 }
