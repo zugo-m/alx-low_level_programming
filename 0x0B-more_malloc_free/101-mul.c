7efbdfa9879e96781e6747daf3f4a839d8e91f7d
lroudge <lauradrc@hotmail.fr>
2019-03-08 01:48:57 +0000

    Find the error in task 5

--- 0x0B-more_malloc_free/101-mul.c
+++ 0x0B-more_malloc_free/101-mul.c
@@ -39,6 +39,15 @@ int _strlen(char *s)
 }
 
 /**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -48,19 +57,11 @@ int _strlen(char *s)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len1, len2, len, i, carry, digit1, digit2, *result;
+	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
-	if (argc != 3)
-	{
-		printf("Error\n");
-		exit(98);
-	}
 	s1 = argv[1], s2 = argv[2];
-	if (!is_digit(s1) || !is_digit(s2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
@@ -73,7 +74,7 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = len2 - 1; len2 >= 0; len2--)
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
@@ -84,7 +85,14 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] += carry;
 	}
 	for (i = 0; i < len - 1; i++)
-		_putchar(result[i] + '0');
+	{
+		if (result[i])
+			a = 1;
+		if (a)
+			_putchar(result[i] + '0');
+	}
+	if (!a)
+		_putchar('0');
 	_putchar('\n');
 	return (0);
 }
