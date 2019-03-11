898ceaa0cf453181bf55c40e7739554c2cae4dd5
lroudge <lauradrc@hotmail.fr>
2019-03-11 20:45:34 +0000

    Add free() in tasks 4 and 5

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -72,11 +72,17 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
+	{
+		free(dog);
 		return (NULL);
+	}
 	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
+	{
+		free(dog);
+		free(dog->name);
 		return (NULL);
-
+	}
 	_strcpy(dog->name, name);
 	_strcpy(dog->owner, owner);
 	dog->age = age;
