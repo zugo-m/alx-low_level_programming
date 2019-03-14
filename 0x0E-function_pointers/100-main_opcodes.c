78bb0ae1815b840d8ab3b973b570474cb9632984
lroudge <lauradrc@hotmail.fr>
2019-03-14 01:11:44 +0000

    Add functionnality to task 4

--- 0x0E-function_pointers/100-main_opcodes.c
+++ 0x0E-function_pointers/100-main_opcodes.c
@@ -10,7 +10,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int bytes;
+	int bytes, i;
+	char *arr;
 
 	if (argc != 2)
 	{
@@ -25,6 +26,17 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(2);
 	}
+	
+	arr = (char *)main;
 
+	for (i = 0; i < bytes; i++)
+	{
+		if (i == bytes - 1)
+		{
+			printf("%02hhx\n", arr[i]);
+			break;
+		}
+		printf("%02hhx ", arr[i]);
+	}
 	return (0);
 }
