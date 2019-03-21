cca6ec45e70c3dfc4155a6c1be5b6ff0d2c25d22
lroudge <lauradrc@hotmail.fr>
2019-03-21 19:26:04 +0000

    Remove an argument from task 6

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -3,12 +3,10 @@
 
           section   .text
 main:
-          mov	esi, sentence
 	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
 	  ret
 
 	  section .data
-format: db `%s\n`,0
-sentence: db "Hello, Holberton",0
+format: db `Hello, Holberton\n`,0
