992de82335562a4ce75019ca1b35ea3dac2abe63
lroudge <lauradrc@hotmail.fr>
2019-03-21 19:22:20 +0000

    Little fix in task 6

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -5,10 +5,10 @@
 main:
           mov	esi, sentence
 	  mov   edi, format
-	  xor eax, eax
-	  call printf
+	  xor   eax, eax
+	  call  printf
 	  ret
 
 	  section .data
-format: db  `%s\n`,0
+format: db `%s\n`,0
 sentence: db "Hello, Holberton",0
