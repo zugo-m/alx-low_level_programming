029384cd0c11e2ade24c9d1e4f224cd41968554a
lroudge <lauradrc@hotmail.fr>
2019-03-21 18:15:41 +0000

    Add advanced task 6

---
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,14 @@
+          global    main
+          extern    printf
+
+          section   .text
+main:
+          mov	rsi, sentence
+	  mov   rdi, format
+	  xor rax, rax
+	  call printf
+	  ret
+
+	  section .data
+format: db  `%s\n`, 0
+sentence: db "Hello, Holberton", 0
