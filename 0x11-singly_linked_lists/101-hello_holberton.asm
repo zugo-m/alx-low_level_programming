e1e87f496d3c376f6cc7a0f65f28d2c3fc97dba3
lroudge <lauradrc@hotmail.fr>
2019-03-21 19:28:12 +0000

    Refactor code for task 6

--- 0x11-singly_linked_lists/101-hello_holberton.asm
+++ 0x11-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,9 @@
           global    main
           extern    printf
-
           section   .text
 main:
 	  mov   edi, format
 	  xor   eax, eax
 	  call  printf
 	  ret
-
-	  section .data
 format: db `Hello, Holberton\n`,0
