423b8a98bae593ff9637aba9cfda4e7809e43a4f
lroudge <lauradrc@hotmail.fr>
2019-03-25 19:42:21 +0000

    Add precision to the documentation on task 2 and 6

--- 0x12-more_singly_linked_lists/2-add_nodeint.c
+++ 0x12-more_singly_linked_lists/2-add_nodeint.c
@@ -5,7 +5,7 @@
  * @head: pointer to the first node in the list
  * @n: data to insert in that new node
  *
- * Return: pointer to the new node
+ * Return: pointer to the new node, or NULL if it fails
  */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
