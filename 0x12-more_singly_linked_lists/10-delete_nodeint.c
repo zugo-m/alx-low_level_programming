aee001380f7dfa0c57105f5c0e1188ce7ece3ee9
lroudge <lauradrc@hotmail.fr>
2019-03-25 19:56:54 +0000

    Modify task 10

--- 0x12-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x12-more_singly_linked_lists/10-delete_nodeint.c
@@ -10,7 +10,7 @@
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp = *head;
-	listint_t *next = NULL;
+	listint_t *current = NULL;
 	unsigned int i = 0;
 
 	if (*head == NULL)
@@ -25,16 +25,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	while (i < index - 1)
 	{
+		if (!temp || !(temp->next))
+			return (-1);
 		temp = temp->next;
 		i++;
 	}
 
-	if (!temp || !(temp->next))
-		return (-1);
 
-	next = temp->next;
-	temp->next = next->next;
-	free(next);
+	current = temp->next;
+	temp->next = current->next;
+	free(current);
 
 	return (1);
 }
