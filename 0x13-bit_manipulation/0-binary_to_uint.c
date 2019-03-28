5e60097867d56be0065d5e3b579a08a8b6f20978
lroudge <lauradrc@hotmail.fr>
2019-03-28 16:27:45 +0000

    Initial commit of project 0x13 C

---
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,79 @@
+#include "holberton.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted
+ *
+ * Return: the int converted from the string
+ */
+int _atoi(const char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++;
+	}
+
+	if (f == 0)
+		return (0);
+
+	return (n);
+}
+
+/**
+ * binary_to_uint - converts a binary number to unsigned int
+ * @b: string containing the binary number
+ *
+ * Return: the converted number
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int num, last_digit, i, base = 1;
+	unsigned int dec_val = 0;
+
+	if (!b)
+		return (0);
+
+	for (i = 0; b[i]; i++)
+	{
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+	}
+
+	num = _atoi(b);
+
+	while (num)
+	{
+		last_digit = num % 10;
+		num /= 10;
+		dec_val += last_digit * base;
+		base *= 2;
+	}
+
+	return (dec_val);
+}
