1f6602bd59c94f8bf9ad43c7d3c8fd67ad93c9e1
lroudge <lauradrc@hotmail.fr>
2019-03-28 19:23:53 +0000

    Refactor task 0

--- 0x13-bit_manipulation/0-binary_to_uint.c
+++ 0x13-bit_manipulation/0-binary_to_uint.c
@@ -1,51 +1,6 @@
 #include "holberton.h"
 
 /**
- * _atoi - converts a string to an integer
- * @s: string to be converted
- *
- * Return: the int converted from the string
- */
-int _atoi(const char *s)
-{
-	int i, d, n, len, f, digit;
-
-	i = 0;
-	d = 0;
-	n = 0;
-	len = 0;
-	f = 0;
-	digit = 0;
-
-	while (s[len] != '\0')
-		len++;
-
-	while (i < len && f == 0)
-	{
-		if (s[i] == '-')
-			++d;
-
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			digit = s[i] - '0';
-			if (d % 2)
-				digit = -digit;
-			n = n * 10 + digit;
-			f = 1;
-			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break;
-			f = 0;
-		}
-		i++;
-	}
-
-	if (f == 0)
-		return (0);
-
-	return (n);
-}
-
-/**
  * binary_to_uint - converts a binary number to unsigned int
  * @b: string containing the binary number
  *
@@ -53,7 +8,7 @@ int _atoi(const char *s)
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int num, last_digit, i, base = 1;
+	int i;
 	unsigned int dec_val = 0;
 
 	if (!b)
@@ -63,16 +18,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] < '0' || b[i] > '1')
 			return (0);
-	}
-
-	num = _atoi(b);
-
-	while (num)
-	{
-		last_digit = num % 10;
-		num /= 10;
-		dec_val += last_digit * base;
-		base *= 2;
+		dec_val = 2 * dec_val + (b[i] - '0');
 	}
 
 	return (dec_val);
