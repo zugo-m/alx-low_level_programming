4a8c40251acedd9cbcc7950e0e68bcaed6e3baff
lroudge <lauradrc@hotmail.fr>
2019-04-02 19:54:17 +0000

    Modify order of error checking

--- 0x14-file_io/3-cp.c
+++ 0x14-file_io/3-cp.c
@@ -17,19 +17,21 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
 	fd_r = open(argv[1], O_RDONLY);
 	if (fd_r < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
 	fd_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
+	if (fd_w < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 	while ((r = read(fd_r, buf, 1024)) > 0)
 	{
-		if (write(fd_w, buf, r) != r || fd_w < 0)
+		if (write(fd_w, buf, r) != r)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			exit(99);
@@ -38,7 +40,6 @@ int main(int argc, char *argv[])
 
 	a = close(fd_r);
 	b = close(fd_w);
-
 	if (a < 0 || b < 0)
 	{
 		if (a < 0)
@@ -47,6 +48,5 @@ int main(int argc, char *argv[])
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_w);
 		exit(100);
 	}
-
 	return (0);
 }
