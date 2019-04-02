35959eaba0497f58c18f0388d8470f7e250c8b68
lroudge <lauradrc@hotmail.fr>
2019-04-02 19:31:12 +0000

    Change task 1 and 2

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -14,15 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[s])
-		s++;
-
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (fd < 0)
 		return (-1);
 
 	if (text_content)
 	{
+		while (text_content[s])
+			s++;
 		t = write(fd, text_content, s);
 		if (t != s)
 			return (-1);
--- 0x14-file_io/2-append_text_to_file.c
+++ 0x14-file_io/2-append_text_to_file.c
@@ -14,15 +14,14 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	while (text_content[s])
-		s++;
-
 	fd = open(filename, O_WRONLY | O_APPEND);
 	if (fd < 0)
 		return (-1);
 
 	if (text_content)
 	{
+		while (text_content[s])
+			s++;
 		t = write(fd, text_content, s);
 		if (t != s)
 			return (-1);
