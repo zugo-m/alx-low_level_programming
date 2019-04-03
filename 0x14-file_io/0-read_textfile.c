6fa03795739c6feb30e17b9fa81fbdf13e12b6d8
lroudge <lauradrc@hotmail.fr>
2019-04-03 20:33:25 +0000

    Refactor task 0

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,10 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
 	int s, t;
-	char *buf = malloc(sizeof(char) * letters);
-
-	if (!buf)
-		return (0);
+	char *buf;
 
 	if (!filename)
 		return (0);
@@ -23,16 +20,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd < 0)
 		return (0);
 
+	buf = malloc(sizeof(char) * letters);
+	if (!buf)
+		return (0);
+
 	s = read(fd, buf, letters);
 	if (s < 0)
+	{
+		free(buf);
 		return (0);
+	}
 	buf[s] = '\0';
 
 	close(fd);
 
 	t = write(STDOUT_FILENO, buf, s);
 	if (t < 0)
+	{
+		free(buf);
 		return (0);
+	}
 
 	free(buf);
 	return (t);
