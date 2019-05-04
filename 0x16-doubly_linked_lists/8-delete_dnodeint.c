7683832384693dc000b4fa81437d34a4d0f4b85f
lroudge <lauradrc@hotmail.fr>
2019-05-04 19:59:25 +0000

    Correction of task 8

--- 0x16-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,7 +13,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	dlistint_t *temp = *head;
 	unsigned int i = 0;
 
-	if (*head == NULL || !head)
+	if (*head == NULL)
 		return (-1);
 
 	if (index == 0)
