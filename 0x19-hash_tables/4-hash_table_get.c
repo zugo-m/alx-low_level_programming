05f8fcaad3ca8d1f32ba5b970331ae6ed7894d7e
lroudge <lauradrc@hotmail.fr>
2019-06-03 02:54:33 +0000

    Correction of task 4

--- 0x19-hash_tables/4-hash_table_get.c
+++ 0x19-hash_tables/4-hash_table_get.c
@@ -10,12 +10,13 @@
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
-	unsigned long int index = key_index((const unsigned char *)key, ht->size);
+	unsigned long int index;
 	hash_node_t *node = NULL;
 
 	if (!ht || !key || !strcmp(key, ""))
 		return (NULL);
 
+	index = key_index((const unsigned char *)key, ht->size);
 	node = (ht->array)[index];
 
 	while (node)
