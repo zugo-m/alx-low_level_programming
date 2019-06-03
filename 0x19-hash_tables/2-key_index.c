95dae35f2130b08f012e15d43503106185fa8813
lroudge <lauradrc@hotmail.fr>
2019-06-03 23:44:02 +0000

    Change disposition in task 2

--- 0x19-hash_tables/2-key_index.c
+++ 0x19-hash_tables/2-key_index.c
@@ -10,10 +10,12 @@
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-	unsigned long int hash = hash_djb2(key);
+	unsigned long int hash;
 
 	if (size == 0)
 		return (0);
+
+	hash = hash_djb2(key);
 
 	return (hash % size);
 }
