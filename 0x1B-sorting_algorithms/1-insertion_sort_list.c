948b98c7079cec862f9d28a27247cb8680c70b70
lroudge <lauradrc@hotmail.fr>
2019-06-14 22:06:25 +0000

    Add task 1

---
+++ 0x1B-sorting_algorithms/1-insertion_sort_list.c
@@ -0,0 +1,60 @@
+#include "sort.h"
+
+/**
+ * len_list - returns the length of a linked list
+ * @h: pointer to the list
+ *
+ * Return: length of list
+ */
+int len_list(listint_t *h)
+{
+	int len = 0;
+
+	while (h)
+	{
+		len++;
+		h = h->next;
+	}
+	return (len);
+}
+
+/**
+ * insertion_sort_list - sorts a linked list with the Insert Sort algorithm
+ * @list: double pointer to the list to sort
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *curr = *list, *one, *two, *three, *four;
+
+	if (!list || !*list)
+		return;
+	if (len_list(*list) < 2)
+	{
+		print_list(*list);
+		return;
+	}
+	while (1)
+	{
+		while (curr->next && curr->n < curr->next->n)
+			curr = curr->next;
+		if (!curr->next)
+			return;
+		one = curr->prev;
+		two = curr;
+		three = curr->next;
+		four = curr->next->next;
+
+		two->next = four;
+		if (four)
+			four->prev = two;
+		three->next = two;
+		three->prev = two->prev;
+		if (one)
+			one->next = three;
+		else
+			*list = three;
+		two->prev = three;
+		curr = *list;
+		print_list(curr);
+	}
+}
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -2,6 +2,7 @@
 #define SORT_H
 
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -22,5 +23,7 @@ void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
 void bubble_sort(int *array, size_t size);
+int len_list(listint_t *h);
+void insertion_sort_list(listint_t **list);
 
 #endif
