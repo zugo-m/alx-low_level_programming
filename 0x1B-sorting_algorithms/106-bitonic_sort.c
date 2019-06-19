51677bb87c31bbac46b6f5d7b19fe7567f1c6357
lroudge <lauradrc@hotmail.fr>
2019-06-19 20:08:24 +0000

    Add bitonic sort

--- 0x1B-sorting_algorithms/106-bitonic_sort.c
+++ 0x1B-sorting_algorithms/106-bitonic_sort.c
@@ -10,59 +10,66 @@ void bitonic_sort(int *array, size_t size)
 	if (!array || size < 2)
 		return;
 
-	bitonic_recursion(array, 0, size, 1, size);
+	bitonic_recursion(array, 0, size - 1, 1, size);
 }
 
 /**
  * bitonic_recursion - recursive function for bitonic sort
  * @array: array to sort
- * @left: index of the left-most element
- * @s: size of the temporary array
+ * @l: index of the left-most element
+ * @r: index of the right-most element
  * @direction: ascending or descending
+ * @size: size of the array
  */
-void bitonic_recursion(int *array, int left, int s, int direction, size_t size)
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size)
 {
-	int tmp_s;
+	int step;
 
-	if (s > 1)
+	if (r - l >= 1)
 	{
-		tmp_s = s / 2;
-		bitonic_recursion(array, left, tmp_s, 1, size);
-		bitonic_recursion(array, left + tmp_s, tmp_s, 0, size);
-		printf("Merging [%d/%d] ", tmp_s, (int)size);
+		step = (r + l) / 2;
+		printf("Merging [%d/%lu] ", r - l + 1, size);
 		if (direction)
 			printf("(UP):\n");
 		else
-			printf("(DOWN)\n");
-		bitonic_merge(array, left, s, direction);
+			printf("(DOWN):\n");
+		print_array(array + l, r - l + 1);
+		bitonic_recursion(array, l, step, 1, size);
+		bitonic_recursion(array, step + 1, r, 0, size);
+		bitonic_merge(array, l, r, direction);
+		printf("Result [%d/%lu] ", r - l + 1, size);
+		if (direction)
+			printf("(UP):\n");
+		else
+			printf("(DOWN):\n");
+		print_array(array + l, r - l + 1);
 	}
 }
 
 /**
  * bitonic_merge - sorts and merges a sequence in ascending or descending order
  * @array: array to sort
- * @left: index of the left-most element
- * @s: size of the temporary array
+ * @l: index of the left-most element
+ * @r: index of the right-most element
  * @direction: ascending or descending
  */
-void bitonic_merge(int *array, int left, int s, int direction)
+void bitonic_merge(int *array, int l, int r, int direction)
 {
-	int tmp, i, tmp_s = s;
+	int tmp, i, step = (l + r) / 2, mid = (r - l + 1) / 2;
 
-	if (s > 1)
+	if (r - l >= 1)
 	{
-		tmp_s = s / 2;
-		for (i = left; i < left + tmp_s; i++)
+		for (i = l; i < l + mid; i++)
 		{
-			if (direction == (array[i] > array[i + tmp_s]))
+			if (direction == (array[i] > array[i + mid]))
 			{
-				tmp = array[i + tmp_s];
-				array[i + tmp_s] = array[i];
+				tmp = array[i + mid];
+				array[i + mid] = array[i];
 				array[i] = tmp;
 			}
 		}
-		print_array(array, s);
-		bitonic_merge(array, left, tmp_s, direction);
-		bitonic_merge(array, left + tmp_s, tmp_s, direction);
+		bitonic_merge(array, l, step, direction);
+		bitonic_merge(array, step + 1, r, direction);
 	}
+
 }
--- 0x1B-sorting_algorithms/sort.h
+++ 0x1B-sorting_algorithms/sort.h
@@ -42,5 +42,8 @@ void heapify(int *array, size_t s, size_t root, size_t size);
 void radix_sort(int *array, size_t size);
 void count_sort_LSD(int *array, size_t size, size_t lsd);
 void quick_sort_hoare(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void bitonic_recursion(int *array, int l, int r, int direction, size_t size);
+void bitonic_merge(int *array, int l, int r, int direction);
 
 #endif
