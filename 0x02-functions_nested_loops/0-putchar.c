5d2354a897b90b8019615903d59d86faf3c106d4
zugo-m <99685288+zugo-m@users.noreply.github.com>
2022-03-15 19:13:15 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,8 +1,7 @@
-#include "_putchar.h"
 #include "main.h"
 
 /**
- * main -  prints Holberton, followed by a new line
+ * main -  prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
