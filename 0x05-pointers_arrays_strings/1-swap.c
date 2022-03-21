9cdbec1b6d022270309f93797b7a4e17d4748f4b
Marvis <marvelscott19@gmail.com>
2022-03-21 16:44:27 -0700

    Task 1 added swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,13 +1,15 @@
 #include "main.h"
 
 /**
- * swaps the value of integers
- * a and b are pointers of first and second integer type variable that will swap
+ * swap_int - swaps the value of integers
+ * @a: pointer to first value of variable of int type
+ * @b: pointer to second value of variable of int type
  */
 
 void swap_int(int *a, int *b)
 {
 	int y;
+
 	y = *a;
 	*a = *b;
 	*b = y;
