15c2ed4c2a31baae2c3e051d39b6d474fa380a14
Marvis <marvelscott19@gmail.com>
2022-03-21 15:56:23 -0700

    main file updated

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,7 +3,7 @@
 /**
  * reset_to_98 - updates the value of the variable
  * the pointer points to 98
- * n: pointer to the variable that will update
+ * @n: pointer to the variable that will update
  */
 
 void reset_to_98(int *n)
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef FILE_MAIN
+#define FILE_MAIN
+
 void reset_to_98(int *n); 
 void swap_int(int *a, int *b);
 int _strlen(char *s);
