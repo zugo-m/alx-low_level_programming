f7449f1f25fab3bebe8f94847c7c1b1964905c69
Marvis <marvelscott19@gmail.com>
2022-03-25 08:07:27 -0700

    Task 1 added String concatenation with byte size of source string condition

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/** _strncat - concatenates two strings
+ * @dest: string to append to
+ * @src: string to append
+ * @n: byte size to add from src
+ * 
+ * Return: pointer to the resulting string dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = 0;
+	j = 0;
+
+	while (dest[i] != '\0')
+		i++;
+
+	while (src[j] != '\0' && j < n)
+
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	
+	dest[i] = '\0';
+
+	return (dest);
+}       	
