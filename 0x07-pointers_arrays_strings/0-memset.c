9486f657b20f4e870ba560a8c7e835da6ada8608
Marvis <marvelscott19@gmail.com>
2022-04-04 00:03:05 +0000

    memset betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -3,7 +3,7 @@
 /**
  * _memset - fills memory with a constant byte
  * @s: Pointer to the n bytes for memory
- * @b: constant byte 
+ * @b: constant byte
  * @n: size of memory
  *
  * Return: a pointer to the memory area s
@@ -12,7 +12,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for( i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		s[i] = b;
 	}
