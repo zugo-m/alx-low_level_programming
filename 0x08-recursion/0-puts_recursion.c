89fec7c071f9d470c4b4aaba6c053d87858355d3
Marvis <marvelscott19@gmail.com>
2022-05-01 06:55:07 +0000

    Task 0 is a function that prints a string followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * _puts_recursion - funciton that prints a string followed by a new line
+ * @s: string to prirnt
+ *
+ * Return: String followed by a new line.
+ */
+void_puts_recursion(char *s)
+{
+	//base case
+	if (*s == '\0')
+	{
+		_putchar("\n");
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s+1);
+}	
