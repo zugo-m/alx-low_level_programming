a147fc2c981296de872549125832b7547219e5ae
Marvis <marvelscott19@gmail.com>
2022-05-02 22:27:54 +0000

    File 2 is a function that return the length of a string

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -4,14 +4,14 @@
  * _strlen_recursion - return the length of a string
  * @s: string to print its length
  * Return: the length of a given string
- * */
+ */
 int _strlen_recursion(char *s)
 {
 	int count = 0;
 
 	if (*s == '\0')
 	{
-		return count;
+		return (count);
 	}
-	return(1 + _strlen_recursion(s + 1));
+	return (1 + _strlen_recursion(s + 1));
 }
