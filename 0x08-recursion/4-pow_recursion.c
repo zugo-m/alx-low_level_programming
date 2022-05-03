7f03482d6c5f37900a0212989ba2bf4c5a4973b5
Marvis <marvelscott19@gmail.com>
2022-05-03 05:51:17 +0000

    File 4 is a function that returns the value of a number raised to the power of another number

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main"
+
+/**
+ * _pow_recursion - function that returns the value of a number raised to the power of another
+ * @x: base value
+ * @y: power to which based value is raised
+ * Return: value of x raused to power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (y == 0)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
