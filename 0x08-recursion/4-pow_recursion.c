b54635a060bb2e9a4688b510d78a4cb1967a7753
Marvis <marvelscott19@gmail.com>
2022-05-03 05:57:27 +0000

    File 4 is a function that returns the value of a number raised to the power of another number

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -3,7 +3,7 @@
 /**
  * _pow_recursion - returns the value of x raised to the power of y
  * @x: value to raise
- * @y: power 
+ * @y: power
  * Return: result of the the value raised
  */
 int _pow_recursion(int x, int y)
